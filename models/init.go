e07c6163eb36cf72459de4b14bca7abc279d3496
José Enrique Narváez Gago <jenarvaezg@gmail.com>
2017-10-21 20:50:33 +0200

    Added env to mongo connection

--- models/init.go
+++ models/init.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"log"
+	"os"
 
 	"github.com/go-bongo/bongo"
 )
@@ -9,9 +10,12 @@ import (
 var connection *bongo.Connection
 
 func connectToMongo() *bongo.Connection {
+
+	mongoURL := os.Getenv("MONGO_URL")
+	mongoDatabase := os.Getenv("MONGO_DATABASE")
 	config := &bongo.Config{
-		ConnectionString: "localhost",
-		Database:         "bongotest",
+		ConnectionString: mongoURL,
+		Database:         mongoDatabase,
 	}
 	var err error
 	connection, err = bongo.Connect(config)
