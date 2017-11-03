126e0204f096403838e4da69333bb8e3a432088c
jenarvaezg <jenarvaezg@gmail.com>
2017-11-03 15:40:14 +0100

    fixed a bug

--- handlers/login.go
+++ handlers/login.go
@@ -11,11 +11,11 @@ import (
 func LoginRequestHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	token, err := auth.GetAuthToken(r.Form)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	if err != nil {
 		utils.ResponseError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(token))
 
